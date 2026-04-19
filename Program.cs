08469cbed77f1a7fc3ecbc971e396c6b7ba794e3
Adams Conan Doyle <3662308525@qq.com>
2026-04-19 12:31:57 +0800

    超声波传感器

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO.Ports;
+class Test{
+    
+    public static void println(Object obj) {
+        Console.WriteLine(obj);
+    }
+    public static void println() {
+        Console.WriteLine();
+    }
+    public static void print(Object obj) {
+        Console.Write(obj);
+    }
+    public static void Main(string[] asd) {
+        SerialPort p = new SerialPort {
+            PortName = "COM8",
+            BaudRate = 9600,
+            DataBits = 8,
+            StopBits = StopBits.One,
+            Parity = Parity.None,
+            ReadTimeout = 1000
+        };
+        try {
+            p.Open();   println("Arduino连接成功");
+            while (true) {
+                string data = p.ReadLine().Trim();
+                if (double.TryParse(data, out double a)) print("\r距离："+a+"  厘米");
+            
+            }
+        }catch(Exception e) {
+            Console.WriteLine("错误发生于->"+e.Message);
+        } finally {
+            if (p.IsOpen) p.Close();
+        }
+    }
+}
